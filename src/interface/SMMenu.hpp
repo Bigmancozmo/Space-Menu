6495325f11768b065ddea91ac7d583474e5ae94e
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-02-15 19:04:35 -0500

    center text

--- src/interface/SMMenu.hpp
+++ src/interface/SMMenu.hpp
@@ -13,7 +13,10 @@ CCLayer* SMMenu::create() {
     auto layer = CCLayer::create();
     layer->setID("spacemenu-ui");
 
+    CCSize screenSize = CCDirector::sharedDirector()->getWinSize();
+
     auto text = CCLabelBMFont::create("Hello, World!", "bigFont.fnt");
+    text->setPosition(screenSize / 2);
     layer->addChild(text);
 
     return layer;
