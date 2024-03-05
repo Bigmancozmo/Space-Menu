e78365d802c6f1ae9f4b0ae2c8e92eb2971473c0
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-03-05 17:57:48 -0500

    WHAT ARE WE SETTING UP CAMERON CAMERON 🗣️🗣️🗣️

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,23 @@
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
+
+class $modify(SMButton, MenuLayer){
+    void onButton(CCObject*){
+        FLAlertLayer::create("Space Menu", "What are we setting up Cameron?", "OK");
+    }
+
+    bool init(){
+        if(!MenuLayer::init()) return false;
+
+        CCMenu* spaceMenu = CCMenu::create();
+        CCSprite* sprite = CCSprite::create("SM_Button.png"_spr);
+        CCMenuItemSpriteExtra* sm_button = CCMenuItemSpriteExtra::create(
+            sprite, this, menu_selector(SMButton::onButton)
+        );
+
+        spaceMenu->addChild(sm_button);
+        this->addChild(spaceMenu);
+        return true;
+    }
+};
