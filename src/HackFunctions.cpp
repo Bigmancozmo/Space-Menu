73889850271e47e68756b88d363c332375715d3f
Bigmancozmo <cozmod275@gmail.com>
2024-04-12 16:20:01 -0400

    Flash on Death functionality

--- src/HackFunctions.cpp
+++ src/HackFunctions.cpp
@@ -3,11 +3,18 @@
 
 #include "hacks/Hacks.h"
 
+#include <iostream>
+
+using namespace std;
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
 	void destroyPlayer(PlayerObject * player, GameObject * p1) {
-		if (!(Hacks::isEnabled(Noclip::hackKey))) {
+		if (Hacks::isEnabled(Noclip::hackKey)) {
+			if (Hacks::isEnabled(NoclipFlashOnDeath::hackKey)) {
+				cout << "flash screen here" << endl;
+			}
+		} else {
 			PlayLayer::destroyPlayer(player, p1);
 		}
 	}
